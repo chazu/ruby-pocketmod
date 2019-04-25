f420ddb3f36f245b4fe2791072f21bdf04fa0696
Chaz <chaz@plaidpotion.com>
2019-04-24 21:41:21 -0400

    WIP margins

--- lib/pocketmod.rb
+++ lib/pocketmod.rb
@@ -6,6 +6,8 @@ module Pocketmod
   class Error < StandardError; end
 end
 
+######################
+
 a = Pocketmod::Pocketmod.new
 
 #a.add_panel Pocketmod::Panel.new "foobar"
@@ -13,11 +15,17 @@ a = Pocketmod::Pocketmod.new
 b = Pocketmod::Panel.new text: "lol"
 c = Pocketmod::Panel.new
 
-c.draw do |ctx|
-  ctx.text "DIS IS DA REAL THING YO WIGGITY WACK IM HIP AND WITH IT"
-end
+# c.draw do |ctx|
+#   ctx.text "DIS IS DA REAL THING YO WIGGITY WACK IM HIP AND WITH IT"
+# end
 
 a.add_panel b
 a.add_panel c
 
+a.add_panel Pocketmod::Panel.new text: "lol2"
+a.add_panel Pocketmod::Panel.new text: "lol2"
+a.add_panel Pocketmod::Panel.new text: "lol2"
+# a.add_panel Pocketmod::Panel.new text: "lol2"
+# a.add_panel Pocketmod::Panel.new text: "lol2"
+# a.add_panel Pocketmod::Panel.new text: "lol2"
 a.generate
--- lib/pocketmod/pocketmod.rb
+++ lib/pocketmod/pocketmod.rb
@@ -6,13 +6,10 @@ module Pocketmod
     POINTS_H = 792
     POINTS_V = 612
 
-    PANEL_MARGIN = 2
+    PANEL_MARGIN = 10
 
     PANEL_HEIGHT = (POINTS_V / 2) - (PANEL_MARGIN * 2)
     PANEL_WIDTH  = (POINTS_H / 4) - (PANEL_MARGIN * 2)
-    
-    PANEL_INNER_HEIGHT = (POINTS_V / 2) - (PANEL_MARGIN * 2)
-    PANEL_INNER_WIDTH  = (POINTS_H / 4) - (PANEL_MARGIN * 2)
 
     def initialize
       @panels = []
@@ -23,10 +20,19 @@ module Pocketmod
     end
 
     def generate
-      Prawn::Document.generate "pocketmod.pdf", page_layout: :landscape, margin: 1 do |pdf|
+      Prawn::Document.generate "pocketmod.pdf", page_layout: :landscape, margin: 0 do |pdf|
         @panels.each do |panel|
+          puts panel_position(panel)
+          puts "==="
           pdf.rotate rotate_panel?(panel) ? 180 : 0, origin: panel_center(panel) do
-            pdf.bounding_box panel_position(panel), width: PANEL_WIDTH, height: PANEL_HEIGHT do
+            pdf.bounding_box panel_position(panel),
+                             width:  PANEL_WIDTH,
+                             height: PANEL_HEIGHT,
+                             margin: PANEL_MARGIN  do
+
+              # Draw a border around the panel
+              pdf.stroke_bounds
+
               if panel.has_block?
                 panel.render pdf
               else
@@ -42,12 +48,12 @@ module Pocketmod
 
     def panel_x panel
       index = @panels.find_index(panel)
-      ((index % 4)) * PANEL_WIDTH
+      (((index % 4)) * PANEL_WIDTH) + PANEL_MARGIN
     end
 
     def panel_y panel
       index = @panels.find_index(panel)
-      index <= 3 ? POINTS_V / 2 : 0
+      index <= 3 ? (POINTS_V / 2 + PANEL_MARGIN) : (0 + PANEL_MARGIN)
     end
 
     def panel_position panel
